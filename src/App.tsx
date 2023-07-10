563e83b7a37223d494368fd178e942e0a11859e7
John Dickerson <john.charles.dickerson@gmail.com>
2023-07-10 10:49:06 +0300

    Added tysconfig.json
    Also added  dev dependency:
    @babel/plugin-proposal-private-property-in-object

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { addressSignatureVerification } from "@ututrust/web-components";
 
 import { useWeb3Modal } from '@web3modal/react'
 
-const { open, close } = useWeb3Modal()
-
 // A list of offers to be shown to the user, such as a list of products in an e-commerce app or a list of service
 // providers in a sharing economy app. This would typically be retrieved from the app's backend.
 const OFFERS = [
@@ -27,6 +25,7 @@ const OFFERS = [
 
 
 function App() {
+  const { open, close } = useWeb3Modal()
   const [hasToken, setHasToken] = useState(false);
 
   let _window: any = window;
