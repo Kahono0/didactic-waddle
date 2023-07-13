26f40f819d451ba9bd9f6bb5f4fd98732570093f
John Dickerson <john.charles.dickerson@gmail.com>
2023-07-13 21:33:46 +0300

    Formatted config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -25,22 +25,29 @@ module.exports = function override(config, env) {
   //  }
   //}));
 
-  config.plugins = config.plugins.filter(function (plugin) { return !(plugin instanceof MiniCssExtractPlugin); });
-  config.module.rules = config.module.rules.map(function (moduleRule) {
-    var _a;
-    moduleRule.oneOf = (_a = moduleRule.oneOf) === null || _a === void 0 ? void 0 : _a.map(function (rule) {
-      if (!rule.hasOwnProperty('use'))
-        return rule;
-      return Object.assign({}, rule, {
-        use: rule.use.map(function (options) {
-          return /mini-css-extract-plugin/.test(options.loader)
-            ? { loader: require.resolve('style-loader'), options: {} }
-            : options;
-        })
-      });
+  config.plugins = config.plugins.filter(
+    function (plugin) {
+      return !(plugin instanceof MiniCssExtractPlugin);
+    }
+  );
+
+  config.module.rules = config.module.rules.map(
+    function (moduleRule) {
+      var _a;
+      moduleRule.oneOf = (_a = moduleRule.oneOf) === null || _a === void 0 ? void 0 : _a.map(
+        function (rule) {
+          if (!rule.hasOwnProperty('use'))
+            return rule;
+          return Object.assign({}, rule, {
+            use: rule.use.map(function (options) {
+              return /mini-css-extract-plugin/.test(options.loader)
+                ? { loader: require.resolve('style-loader'), options: {} }
+                : options;
+            })
+          });
+        });
+      return moduleRule;
     });
-    return moduleRule;
-  });
 
   return config;
 }
