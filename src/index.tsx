07e3dccde8853b27db811234796177805b42c6e7
John Dickerson <john.charles.dickerson@gmail.com>
2023-07-13 16:27:12 +0300

    Added Web3Modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import { Web3Modal } from '@web3modal/react'
 import reportWebVitals from './reportWebVitals';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
@@ -9,16 +11,19 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 
 const chains = [arbitrum, mainnet, polygon];
-const projectId = "5da12abf6f18a40401bebb83bbbedffe" // process.env.customKey
+// const projectId = "5da12abf6f18a40401bebb83bbbedffe" // process.env.customKey
+
+// jd's one from https://cloud.walletconnect.com/
+const projectId = "ed0689b91e26b121d6a6145437c7b857";
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-
-
 
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, chains }),
   publicClient
 });
+
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 //const root = ReactDOM.createRoot(document.getElementById('root')!);
 const root = createRoot(document.getElementById('root')!);
@@ -27,6 +32,8 @@ root.render(
     <WagmiConfig config={wagmiConfig}>
       <App />
     </WagmiConfig>
+
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
   </React.StrictMode>
 );
 
